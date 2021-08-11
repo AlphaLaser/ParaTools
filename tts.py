8c63c08df570b3b47cfb6e83ce262f126684cd7e
AlphaLaser <59497243+AlphaLaser@users.noreply.github.com>
2021-08-11 15:55:08 +0530

    Update tts.py

--- tts.py
+++ tts.py
@@ -1,5 +1,6 @@
 from gtts import gTTS
 import streamlit as st
+import random
 
 # The text that you want to convert to audio
 def tts(mytext):
@@ -10,9 +11,19 @@ def tts(mytext):
 
     # Saving the converted audio in a mp3 file named
     # welcome
-    myobj.save("audio/welcome.mp3")
 
+    rand_string_list = []
+
+    count = 0
+    while count <= 10 :
+        a = random.randint(0,9)
+        rand_string_list.append(str(a))
+        count += 1
+
+    rand_string = "".join(rand_string_list)
+    myobj.save("audio/" + "welcome.mp3" + str(rand_string))
+    file_name = ("audio/" + "welcome.mp3" + str(rand_string))
     # Playing the converted file
-    audio_file = open('audio/welcome.mp3', 'rb')
+    audio_file = open(file_name, 'rb')
     audio_bytes = audio_file.read()
     st.audio(audio_bytes, format='audio/ogg')
